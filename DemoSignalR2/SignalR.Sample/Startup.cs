f01bf5cde7c210c8a63aef8e21e0135c0cc0935e
spilote72 <spilote72@gmail.com>
2016-08-24 17:32:10 -0400

    Initial commit

---
+++ DemoSignalR2/SignalR.Sample/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Owin;
+using Microsoft.Owin;
+
+[assembly: OwinStartup(typeof(Microsoft.AspNet.SignalR.StockTicker.Startup), "Configuration")]
+
+namespace Microsoft.AspNet.SignalR.StockTicker
+{
+    public static class Startup
+    {
+        public static void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application using OWIN startup, visit http://go.microsoft.com/fwlink/?LinkID=316888
+
+             app.MapSignalR();
+        }
+    }
+}
